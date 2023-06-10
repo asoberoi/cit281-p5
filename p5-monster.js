974ca9692781bcb200a58fe47426bcd900a50cd9
asoberoi <134101380+asoberoi@users.noreply.github.com>
2023-06-09 17:46:53 -0700

    Add files via upload

---
+++ p5-monster.js
@@ -0,0 +1,29 @@
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  class Monster {
+    constructor({ monsterName = "Unknown", minimumLife = 0, currentLife = 100 }) {
+      this.monsterName = monsterName;
+      this.minimumLife = minimumLife;
+      this.currentLife = currentLife;
+      this.isAlive = this.currentLife >= this.minimumLife;
+    }
+  
+    updateLife(lifeChangeAmount) {
+      this.currentLife += lifeChangeAmount;
+      this.currentLife = this.currentLife < 0 ? 0 : this.currentLife;
+      this.isAlive = this.currentLife >= this.minimumLife;
+    }
+  
+    randomLifeDrain(minimumLifeDrain, maximumLifeDrain) {
+      const lifeChangeAmount = getRandomInteger(minimumLifeDrain, maximumLifeDrain + 1);
+      this.updateLife(-lifeChangeAmount);
+      console.log(
+        `${this.monsterName} drained ${lifeChangeAmount} life. Current life: ${this.currentLife}.`
+      );
+    }
+  }
+  
+  module.exports = Monster;
+  
